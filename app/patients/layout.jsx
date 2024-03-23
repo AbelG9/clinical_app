78c9354e4fface6d546054b7cfb7f4b47f750430
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2024-03-23 16:05:41 -0500

    added breadcrumb

--- app/patients/layout.jsx
+++ app/patients/layout.jsx
@@ -1,13 +1,17 @@
 'use client'
 
 import Navbar from '@/components/Navbar'
+import BreadCrumb from '@/components/BreadCrumb'
 
 export default function PatientsLayout({ children }) {  return (
     <>
       <main className="flex flex-col items-center min-h-screen">
         <Navbar />
-        <div className="container pt-24">
-          {children}
+        <div className="container pt-24 flex flex-col">
+          <BreadCrumb className="flex"/>
+          <div className='mt-4'>
+            {children}
+          </div>
         </div>
       </main>
     </>
